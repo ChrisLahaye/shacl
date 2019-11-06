8d3e5ac68f30f687080abebd201a0255fd0543a0
Chris Lahaye <dev@chrislahaye.com>
2019-11-06 12:58:44 +0100

    Add recursive support to AndConstraintComponent
    Signed-off-by: Chris Lahaye <dev@chrislahaye.com>

--- src/main/java/org/topbraid/shacl/validation/java/AndConstraintExecutor.java
+++ src/main/java/org/topbraid/shacl/validation/java/AndConstraintExecutor.java
@@ -4,8 +4,10 @@ import java.util.Collection;
 
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.RDFNode;
+import org.apache.jena.rdf.model.Resource;
 import org.topbraid.shacl.engine.Constraint;
 import org.topbraid.shacl.validation.ValidationEngine;
+import org.topbraid.shacl.vocabulary.RSH;
 
 class AndConstraintExecutor extends AbstractShapeListConstraintExecutor {
 
@@ -20,13 +22,33 @@ class AndConstraintExecutor extends AbstractShapeListConstraintExecutor {
 		long startTime = System.currentTimeMillis();
 
 		for(RDFNode focusNode : focusNodes) {
+			boolean valueNodeFailed = false;
+			boolean valueNodeUnknown = false;
+
+			value:
 			for(RDFNode valueNode : engine.getValueNodes(constraint, focusNode)) {
-				if(shapes.stream().anyMatch(shape -> {
+				for (Resource shape : shapes) {
 					Model nestedResults = hasShape(engine, constraint, focusNode, valueNode, shape, true);
-					return nestedResults != null;
-				})) {
-					engine.createValidationResult(constraint, focusNode, valueNode, () -> "Value does not have all the shapes in the sh:and enumeration");
+
+					if (engine.getAssignment() != null) {
+						if (nestedResults.contains(shape.asResource(), RSH.No, valueNode)) {
+							valueNodeFailed = true;
+							continue value;
+						}
+						if (nestedResults.contains(shape.asResource(), RSH.Unknown, valueNode)) {
+							valueNodeUnknown = true;
+						}
+					} else if (nestedResults != null) {
+						engine.createValidationResult(constraint, focusNode, valueNode,
+								() -> "Value does not have all the shapes in the sh:and enumeration");
+						continue value;
+					}
 				}
+			}
+
+			if (engine.getAssignment() != null) {
+				engine.getReport().getModel().add(constraint.getShapeResource(),
+						valueNodeFailed ? RSH.No : (valueNodeUnknown ? RSH.Unknown : RSH.Yes), focusNode);
 			}
 		}
 
